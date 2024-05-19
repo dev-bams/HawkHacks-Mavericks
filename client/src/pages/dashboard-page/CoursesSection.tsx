22c966cd7fb690f0b61b862c2f24a9212f9fc2f5
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-05-18 22:28:08 -0400

    stashed

--- client/src/pages/dashboard-page/CoursesSection.tsx
+++ client/src/pages/dashboard-page/CoursesSection.tsx
@@ -1,7 +1,9 @@
+import SearchBar from "./SearchBar";
+
 function CoursesSection() {
   return (
     <div className="border-2 border-black grow h-full min-h-lvh bg-light-purple">
-      CoursesSection
+      <SearchBar />
     </div>
   );
 }
