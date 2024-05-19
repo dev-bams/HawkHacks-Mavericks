cb407c7e580c7e89908d5e5128356c933f6b0645
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-05-19 05:48:44 -0400

    added few screens

--- client/src/pages/dashboard-page/CourseItems.tsx
+++ client/src/pages/dashboard-page/CourseItems.tsx
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
+
 function CourseItems() {
   return (
-    <div className="flex border-[3px] border-black justify-between p-4">
-      <div className="flex">
-        <img />
-        <div>
-          <p>Advanced Cybersecurity Techniques</p>
+    <Link to="/coursedetails">
+      <div className="flex border-[3px] border-black justify-between p-4">
+        <div className="flex">
+          <img />
+          <div>
+            <p>Advanced Cybersecurity Techniques</p>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <p>Total: </p>
+          <p>80hrs</p>
         </div>
       </div>
-      <div className="flex flex-col">
-        <p>Total: </p>
-        <p>80hrs</p>
-      </div>
-    </div>
+    </Link>
   );
 }
 
--- client/src/pages/dashboard-page/SideBar.tsx
+++ client/src/pages/dashboard-page/SideBar.tsx
@@ -3,7 +3,7 @@ import { SideBarProps } from "../../types/types";
 
 function SideBar({ onclick }: SideBarProps) {
   return (
-    <div className="bg-chelsea w-[271px]  min-h-lvh border-2 border-primary h-full">
+    <div className="bg-chelsea min-w-[274px]  min-h-lvh border-2 border-primary h-full">
       <SideBarLinksList onclick={onclick} />
     </div>
   );
