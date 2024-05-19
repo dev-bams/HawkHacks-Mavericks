1210150f95ffe487cc4b2963a906180645df73bb
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-05-19 05:59:55 -0400

    added few screens

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        "clash-display": ["clashDisplay", "sans-serif"],
+      },
       colors: {
         primary: "#000",
         chelsea: "#3300ff",
